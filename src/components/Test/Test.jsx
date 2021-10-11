3b56af5c96fdaf130565e53844bb778c75908a07
tothricsaj <tothricsaj@gmail.com>
2021-10-11 16:10:24 +0200

    conf: react testing library is added

---
+++ src/components/Test/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Test(props) {
+	return (
+		<div>
+			<h2>{ props.title }</h2>
+		</div>
+	)
+}
---
+++ src/components/Test/__test__/Test.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Test from '../Test'
+
+const EXPECTED_VALUE = 'Test title';
+
+describe('Test component to see config', () => {
+	it('should render title with props text', () => {
+		render(<Test title={EXPECTED_VALUE} />);
+		const headingElement = screen.getByText(/Test title/i);
+
+		expect(headingElement).toBeInTheDocument();	
+	});
+});
