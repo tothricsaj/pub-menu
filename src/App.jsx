fc31ead3d3f25e289f1ee6f37523e75efdda6aff
tothricsaj <tothricsaj@gmail.com>
2021-11-17 19:49:48 +0100

    chore: deleted menuList form App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,8 @@ import React from 'react';
 
 import Menu from './components/Menu/Menu';
 
-const menuList = [
-	{
-		name: 'menu 1',
-		price: '11.99'
-	},
-	{
-		name: 'menu 2',
-		price: '12.99'
-	},	{
-		name: 'menu 3',
-		price: '13.99'
-	},
-];
-
 const App = () => (
-  <Menu menuList={menuList} />
+  <Menu />
 );
 
 export default App;
